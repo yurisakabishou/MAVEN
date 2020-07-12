c2ea70ac45c927dcdbc936af04ecae13399b3a2a
yurisakabishou <395147048@qq.com>
2020-07-12 16:58:02 +0800

    11111111

--- src/main/java/com/sunyard/App.java
+++ src/main/java/com/sunyard/App.java
@@ -4,10 +4,12 @@ package com.sunyard;
  * Hello world!
  *
  */
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println(1);
     }
 }
